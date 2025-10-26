10a84052ccd03d3c0a3c57993ffb7fe4f6ad17a7
John Mckay <johnmckay@Johns-Laptop.local>
2025-10-26 17:02:37 +0000

    footer

--- ui/src/app/components/footer/footer.ts
+++ ui/src/app/components/footer/footer.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
-  imports: [],
+  standalone: true,
+  imports: [RouterLink],
   templateUrl: './footer.html',
-  styleUrl: './footer.scss',
+  styleUrls: ['./footer.scss']
 })
 export class Footer {
-
 }
